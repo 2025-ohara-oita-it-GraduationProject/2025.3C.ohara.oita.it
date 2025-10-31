11841f3d53af76f32d7d40ca4ad1116db1edfe93
Halle1ujah <ooi2472042@stu.o-hara.ac.jp>
2025-10-31 10:55:10 +0900

    url_link
    u

--- telles/urls.py
+++ telles/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'telles'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.index, name='index'),  # トップページ
+    path('student/create/', views.student_create, name='student_create'),
+    path('attendance/', views.attendance_list, name='attendance_list'),  # 出欠簿ページ
+    path('logout/', views.logout_view, name='logout'),  # ログアウト処理
+    path('login/', views.login_view, name='login'), 
 ]
--- telles/views.py
+++ telles/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
+from django.contrib.auth import logout
 
-# Create your views here.
-class IndexView(TemplateView):
-    template_name = 'index.html'
+    
+def index(request):
+    return render(request, 'index.html', {'username': request.user.username})
+
+def student_create(request):
+    return render(request, 'student_create.html')
+
+def attendance_list(request):
+    return render(request, 'attendance_list.html', {'username': request.user.username})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('telles:index')
+        else:
+            messages.error(request, 'ユーザー名またはパスワードが間違っています')
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('telles:login')
