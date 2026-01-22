d16c4bc20402b211cddcc066e8e4042ffd3e9a77
hikari2222 <kt.era.shall.hope@gmail.com>
2026-01-22 15:31:05 +0900

    デプロイ用

--- Attendance/settings.py
+++ Attendance/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6f7go)yvqv@u0+q0&wff6xmyb1e2xu_05ug7b&09i(fw!$u5%i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -75,11 +76,14 @@ WSGI_APPLICATION = 'Attendance.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+    )
 }
 
 
@@ -120,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- dockerfile
+++ dockerfile
@@ -1,27 +1,14 @@
-# ベースイメージを指定
 FROM python:3.11-slim
-
-# 環境変数を設定
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
-
-# システムパッケージを更新し、mysqlclientのビルドに必要なライブラリをすべてインストール
 RUN apt-get update && apt-get install -y \
     gcc \
     default-libmysqlclient-dev \
     pkg-config \
     && rm -rf /var/lib/apt/lists/*
-
-# 作業ディレクトリを作成して設定
 WORKDIR /app
-
-# 必要なパッケージをインストール
 COPY requirements.txt /app/
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
-
-# プロジェクトファイルをコピー
 COPY . /app/
-
-# Webサーバー(Gunicorn)を起動
 CMD ["gunicorn", "--bind", "0.0.0.0:8000", "Attendance.wsgi:application"]
